505a736b0c41b7b6feeba2c5aba8e2bdc7403a72
JulianoANR <juliano.appe@gmail.com>
2021-09-14 22:56:41 -0300

    connect

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    
+    //essa linha n é sempre necessaria, ela relaciona manualmante esse model a table post no sql
+    protected $table = 'posts';
+}
